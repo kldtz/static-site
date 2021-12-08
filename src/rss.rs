89ec67831bf6405f143e30f4c95ba4d4170c4216
Tobias Kolditz <tbs.kldtz@gmail.com>
2021-12-08 12:12:05 +0100

    Remove rss dependency

--- src/rss.rs
+++ src/rss.rs
@@ -1,34 +1,43 @@
 //! Generation of RSS feed.
-use rss::{ChannelBuilder, GuidBuilder, Item, ItemBuilder};
-
+use askama::Template;
 use crate::page::{collect_sorted_configs, PageConfig};
 use crate::SsgResult;
+
+struct ChannelItem<'a> {
+    title: &'a str,
+    description: &'a Option<String>,
+    sub_url: &'a str,
+    date: String,
+}
+
+#[derive(Template)]
+#[template(path = "rss_template.xml", escape = "none")]
+struct RssTemplate<'a> {
+    title: &'a str,
+    url: &'a str,
+    items: Vec<ChannelItem<'a>>,
+}
 
 pub fn generate_feed(url: &str, title: &str) -> SsgResult<String> {
     let configs: Vec<(PageConfig, String)> = collect_sorted_configs()?;
 
-    let mut items: Vec<Item> = Vec::new();
+    let mut items: Vec<ChannelItem> = Vec::new();
     for (i, (c, sub_url)) in configs.iter().enumerate() {
         if i > 9 {
             break;
         }
-        let url = format!("{}/posts/{}", url, sub_url);
-        let item: Item = ItemBuilder::default()
-            .title(c.title.to_string())
-            .description(c.description.clone())
-            .link(url.clone())
-            .guid(GuidBuilder::default().value(url).build()?)
-            .pub_date(c.date.to_rfc2822())
-            .build()?;
+        let item = ChannelItem {
+            title: &c.title,
+            description: &c.description,
+            sub_url: &sub_url,
+            date: c.date.to_rfc2822(),
+        };
         items.push(item);
     }
-
-    let channel = ChannelBuilder::default()
-        .title(title)
-        .link(url)
-        .description("Latest posts")
-        .items(items)
-        .build()?;
-
-    Ok(channel.to_string())
+    let rss = RssTemplate {
+        title: title,
+        url: url,
+        items: items,
+    }.render()?;
+    Ok(rss)
 }
