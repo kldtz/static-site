2f3f263154b73a80d09b75d8292cbf4756cc5655
Tobias Kolditz <tbs.kldtz@gmail.com>
2022-07-10 10:37:52 +0200

    Add description to new post

--- bin/new.sh
+++ bin/new.sh
@@ -9,4 +9,4 @@ title=$1
 dir_name=`echo "${title}" | tr '[:upper:]' '[:lower:]' | tr ' ' '-'`
 md_path="private/content/posts/`date +'%Y'`/${dir_name}"
 mkdir -p $md_path
-printf -- "---\ntitle: \"%s\"\ndate: `date --iso-8601=seconds`\n---" "$title" > "${md_path}/index.md"
+printf -- "---\ntitle: \"%s\"\ndate: `date --iso-8601=seconds`\ndescription: \"%s\"\n---" "$title" "$title" > "${md_path}/index.md"
